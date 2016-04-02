4a80bb333b4387c445cc555918c0cc0c8863e0aa
Daniel Tomlinson <Dan@Tomlinson.io>
2016-04-02 18:28:51 -0500

    Add Venue by identifier

--- lib/bees/venue.ex
+++ lib/bees/venue.ex
@@ -3,7 +3,21 @@ defmodule Bees.Venue do
 
   defstruct id: nil, name: nil, contact: nil, location: nil, categories: nil,
   verified: nil, stats: nil, likes: nil, specials: nil, here_now: nil,
-  referral_id: nil
+  referral_id: nil, photos: nil, groups: nil
+
+  def find_by_identifier(client, identifier) do
+    params = [
+      v: "20160301"
+    ]
+
+    case Bees.Client.get(client, "/venues/#{identifier}", params, false) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+         response = decode_single(body)
+         {:ok, response["response"]["venue"]}
+      {:error, error} ->
+        {:error, error}
+    end
+  end
 
   def search(client, latitude, longitude, limit \\ 10, intent \\ "checkin", radius \\ 800) do
     params = [
@@ -15,7 +29,7 @@ defmodule Bees.Venue do
     ]
     case Bees.Client.get(client, "/venues/search", params, false) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        response = decode(body)
+        response = decode_many(body)
         {:ok,  response["response"]["venues"]}
       {:error, error} ->
         {:error, error}
@@ -24,13 +38,27 @@ defmodule Bees.Venue do
 
   # Private Helpers
 
-  defp decode(body) do
+  defp decode_many(body) do
     mapping = %{
       "response" => %{
-        "venues" => [%Bees.Venue{}]
+        "venues" => [decoder]
       }
     }
 
     Poison.decode!(body, as: mapping)
+  end
+
+  defp decode_single(body) do
+    mapping = %{
+      "response" => %{
+        "venue" => decoder
+      }
+    }
+
+    Poison.decode!(body, as: mapping)
+  end
+
+  defp decoder() do
+    %Bees.Venue{}
   end
 end
