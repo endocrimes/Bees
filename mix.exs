dc01a471eca4e0614b76006d21d403381009250a
Daniel Tomlinson <Dan@Tomlinson.io>
2016-04-02 18:49:14 -0500

    Update mix.exs and LICENSE

--- mix.exs
+++ mix.exs
@@ -2,18 +2,30 @@ defmodule Bees.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bees,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :bees,
+      version: "0.0.1",
+      description: "Foursquare API client for Elixir",
+      elixir: "~> 1.2",
+      package: package,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Daniel Tomlinson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/danieltomlinson/bees" }
+    ]
   end
 
   def application do
     [applications: [:logger, :httpoison, :jsx, :plug, :poison]]
   end
-  
+
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
