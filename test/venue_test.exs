f2f1a530b8846f569ea66fedcff427885e8ea957
Danielle Lancashire <dani@builds.terrible.systems>
2016-04-02 18:28:51 -0500

    Add Venue by identifier

--- test/venue_test.exs
+++ test/venue_test.exs
@@ -17,10 +17,18 @@ defmodule VenueTest do
   end
 
   test "search by ll" do
-    use_cassette "venue" do
+    use_cassette "venue_by_location" do
       {status, body} = Bees.Venue.search(client, 55.2, 44.1, 1, "checkin", 800)
       assert status == :ok
       assert length(body) == 1
+    end
+  end
+
+  test "find by identifier" do
+    use_cassette "venue_by_identifier" do
+      {status, body} = Bees.Venue.find_by_identifier(client, "501fea4de4b05e0d96afc368")
+      assert status == :ok
+      assert body.id == "501fea4de4b05e0d96afc368"
     end
   end
 end
